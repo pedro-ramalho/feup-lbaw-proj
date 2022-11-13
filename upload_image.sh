9b86f69edeff0265f1eed555721785b64082580a
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-11-13 15:07:16 +0000

    Fixed Laraval setup

--- upload_image.sh
+++ upload_image.sh
@@ -3,12 +3,13 @@
 # Stop execution if a step fails
 set -e
 
-IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2122/lbaw21XX # Replace with your group's image name
+IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2223/lbaw22124 # Replace with your group's image name
 
 # Ensure that dependencies are available
 composer install
 php artisan clear-compiled
 php artisan optimize
 
+#docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
