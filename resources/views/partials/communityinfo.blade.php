81320cf5c99aef17370c7f116112477560b6afcb
up202004646 <up202004646@edu.fe.up.pt>
2022-11-20 20:50:45 +0000

    header done

--- resources/views/partials/communityinfo.blade.php
+++ resources/views/partials/communityinfo.blade.php
@@ -1,10 +1,15 @@
 <div id="community-info">
+  <div id="community-banner">
+    <p>Banner</p>
+  </div>
   <section id="community-details">
     <div id="community-img-name">
-      <p>Profile Picture</p>
-      <p>{{ $community->name }}</p>
-      <p>c/{{ $community->name }}</p>
-
+      <img src="{{ asset('img/tux-nightcap.svg') }}">
+      <div id="name_com">
+        <p id="name">{{ $community->name }}</p>
+        <p id="c_slash">c/{{ $community->name }}</p>
+        <p>{{ $community->description }}</p>
+      </div>
     </div>
   </section>
 </div>
