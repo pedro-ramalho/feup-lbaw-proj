6c58c9bf40fbcb0864d2a74c3b717ebbbdc52e94
Fabio Morais <fabiocunhamorais23@gmail.com>
2022-11-20 10:24:01 +0000

    Page almost finished

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\Community;
+use App\Models\Post;
 
 
 class MainController extends Controller
@@ -13,8 +14,8 @@ class MainController extends Controller
     public function show() {
 
         $communities = array($community = Community::find(1),$community = Community::find(2),$community = Community::find(3));
-         
-        return view('pages.main', ['communities' => $communities]);
+        $posts = array($post=Post::find(1),$post=Post::find(2),$post=Post::find(3),$post=Post::find(5));
+        return view('pages.main', ['communities' => $communities], ['posts' => $posts]);
     }
 
     
--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<nav id= mainContent>
 @include('layouts.sort')
 @include('partials.popCommunities', ['communities' => $communities])
-
+<nav id= mainPosts>
+@each('partials.preview_post', $posts, 'post')
+</nav>
+@include('partials.topics')
+</nav>
 @endsection
