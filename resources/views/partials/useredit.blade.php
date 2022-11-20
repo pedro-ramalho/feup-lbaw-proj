a75f4b20fb639f95891efc84d430541b12878c3f
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-11-20 17:12:33 +0000

    Finished redesigning the profile page

--- resources/views/partials/useredit.blade.php
+++ resources/views/partials/useredit.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="{{ route('edit', $user->id) }}">
+<form method="POST" action="{{ route('edit', $user->id) }}" id="edit-profile">
   {{ csrf_field() }}
   <label for="biography">Biography</label>
   <input id="biography" type="text" name="biography" value="{{ $user->biography }}" required autofocus>
