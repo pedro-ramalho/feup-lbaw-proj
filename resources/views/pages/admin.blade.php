4ad3296fcaa0e10e20b1338ce36156c7775edd8d
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-11-23 11:20:58 +0000

    Improved the design of the Admin page

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -13,17 +13,21 @@
 <section id="admin-content">
   <section id="reports">
     <section id="report-posts">
+      <h2>Post Reports</h2>
       <table>
-        <tr>
-          <th>Content ID</th>
-          <th>User ID</th>
-          <th>Reason</th>
-          <th>Date</th>
-        </tr>
-        @each('partials.reportposts', $post__reports, 'report')
+          <tr>
+            <th>Content ID</th>
+            <th>User ID</th>
+            <th>Reason</th>
+            <th>Date</th>
+          </tr>
+        <tbody>
+          @each('partials.reportposts', $post__reports, 'report')
+        </tbody>
       </table>
     </section>
     <section id="report-comments">
+      <h2>Comment Reports</h2>
       <table>
         <tr>
           <th>Content ID</th>
@@ -31,17 +35,21 @@
           <th>Reason</th>
           <th>Date</th>
         </tr>
-        @each('partials.reportposts', $comment__reports, 'report')
+        <tbody>
+          @each('partials.reportposts', $comment__reports, 'report')
+        </tbody>
       </table>
     </section>
   </section>
   <section id="manage-users">
+    <h2>Delete a User</h2>
     <form method="POST" action="{{ route('admin') }}">
       {{ csrf_field() }}
       <label for="username">Username</label>
-        <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
+        <input id="username" type="text" name="username" required autofocus>
       <button type="submit">Delete</button>
     </form>
+    <h2>Create a User</h2>
   </section>
 </section>
 @endsection('content')
