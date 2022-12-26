e940a948f67f14f0925bcc24321e9ff1c5a2d9c7
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-12-26 16:27:46 +0000

    Styled the login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="signin">
-<form method="POST" action="{{ route('login') }}" class="auth-form">
+<section id="signin" class="mt-4 flex flex-col items-center gap-y-4">
+<h1 class="text-3xl ">Sign in to Rabbit</h1>
+<form method="POST" action="{{ route('login') }}" class="auth-form flex flex-col gap-y-2 p-2 text-xl">
     {{ csrf_field() }}
-
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+    
+    <div class="flex items-center gap-x-2">
+        <i class="fa-solid fa-envelope"></i>
+        <label for="email" class="">E-mail</label>
+    </div>
+    <input class="p-1 border-b-2 text-base w-80 bg-white focus:bg-gray-100 focus:outline-none" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
     @if ($errors->has('email'))
         <span class="error">
           {{ $errors->first('email') }}
         </span>
     @endif
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
+    <div class="flex items-center gap-x-2">
+        <i class="fa-solid fa-lock"></i>
+        <label for="password" >Password</label>
+    </div>
+    <input class="p-1 border-b-2 text-base w-80 bg-white focus:bg-gray-100 focus:outline-none" id="password" type="password" name="password" required>
     @if ($errors->has('password'))
         <span class="error">
             {{ $errors->first('password') }}
         </span>
     @endif
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+    <div class="flex items-center items-center gap-x-2 mt-2">
+        <input class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+        <label class="flex items-center gap-x-2">Remember Me</label>
+    </div>
 
-    <div id="signin-signup-container">
-        <button type="submit">
-            Sign in
-        </button>
-        <a class="button button-outline" href="{{ route('register') }}">Register</a>
+    <div id="signin-signup-container" class="flex flex-col items-center gap-y-2 mt-2">
+        <button type="submit" class="p-1 bg-green-500 border border-green-600 hover:bg-green-600 hover:cursor-pointer rounded-md w-full font-semibold text-white">Sign in</button>
+        <a class="button button-outline p-1 border-2 border-gray-300 rounded-md text-base text-center w-full" href="{{ route('register') }}">New to Rabbit? <span class="text-sky-600">Create an account.</span></a>
     </div>
 </form>
 </section>
