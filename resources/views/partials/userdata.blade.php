964e04007f74885cbb04266b54d4be2900f7a45c
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2022-12-29 15:53:06 +0000

    Styled the edit profile page

--- resources/views/partials/userdata.blade.php
+++ resources/views/partials/userdata.blade.php
@@ -43,7 +43,7 @@ function get_favorite_posts(int $id) {
 <div id="user-data" class="mt-8 w-px-896 max-w-4xl flex flex-col gap-y-2">
   @include('layouts.sort')
 
-  <div id="profile-posts" class="flex flex-col w-px-896 max-w-4xl mt-4 gap-y-2">
+  <div id="profile-posts" class="flex flex-col w-px-896 max-w-4xl gap-y-2">
     @each('partials.preview_post', $user->content()->where('is_post', TRUE)->join('post', 'content.id', '=', 'post.id')->get(), 'post')
   </div>
   <div id="profile-comments" class="flex flex-col w-px-896 max-w-4xl gap-y-2">
--- resources/views/partials/useredit.blade.php
+++ resources/views/partials/useredit.blade.php
@@ -1,8 +1,27 @@
-<form method="POST" action="{{ route('edit', $user->id) }}" id="edit-profile">
-  {{ csrf_field() }}
-    <div id="ue-bio">
-      <label for="biography">Biography</label>
-      <textarea id="biography" name="biography" required autofocus> {{ $user->biography }} </textarea>
-    </div>
-    <button type="submit">Save</button>
-</form>
+<div id="edit" class="flex flex-col items-center mt-4 gap-y-4">
+  <h1 class="text-2xl font-bold">Edit your profile</h1>
+  <form method="POST" action="{{ route('edit', $user->id) }}" id="edit-profile" class="flex flex-col gap-y-4">
+    {{ csrf_field() }}
+      <div id="edit-pfp" class="flex flex-col gap-y-2">
+        <div id="edit-pfp-icons" class="flex items-center gap-x-2 text-xl">
+          <i class="fa-solid fa-pen-to-square"></i>
+          <label for="pfp">Edit your profile picture</label>
+        </div>
+        <i class="fa-regular fa-user self-center text-8xl p-2"></i>
+        <input type="file" class="block w-full text-sm text-gray-500
+                                  file:mr-4 file:py-2 file:px-4
+                                  file:rounded-md file:border-0
+                                  file:text-sm file:font-semibold
+                                  file:bg-gray-700 file:text-white
+                                  hover:file:bg-gray-800">
+      </div>
+      <div id="ue-bio" class="flex flex-col gap-y-2">
+        <div id="edit-bio-icons" class="flex items-center gap-x-2 text-xl">
+          <i class="fa-solid fa-book-open"></i>
+          <label for="biography">Edit your biography</label>
+        </div>
+        <textarea id="biography" class="p-2 bg-gray-200 border border-gray-300 rounded-md " name="biography" required autofocus> {{ $user->biography }} </textarea>
+      </div>
+      <button class="p-1 bg-green-500 border border-green-600 hover:bg-green-600 hover:cursor-pointer rounded-md w-full font-semibold text-white" type="submit">Save</button>
+  </form>
+</div>
